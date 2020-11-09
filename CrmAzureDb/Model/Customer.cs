6eaeedf9e05f938a67cec873b5e14462ea078887
Konstantinos Athanasoglou <k.athanasoglou@kaizengaming.com>
2020-11-09 09:51:37 +0200

    add folder

---
+++ CrmAzureDb/Model/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace CrmAzure.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsActive { get; set; }
+
+        // this is a navigation property
+        public List<Order> Orders { get; set; }
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+    }
+}
---
+++ CrmAzureDb/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using CrmAzure.Data;
+using Microsoft.EntityFrameworkCore;
+
+// ReSharper disable All
+
+namespace CrmAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new CrmAzureDbContext())
+            {
+                // fetch customer with id 2
+                var c = db
+                    .Customer
+                    .Where(c => c.Id == 2)
+                    .Include(c => c.Orders)
+                    .ThenInclude(o => o.Products)
+                    .ThenInclude(p => p.Product)
+                    .ToList();
+
+            }
+        }
+    }
+}
